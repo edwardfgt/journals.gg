6ff0e560a70afd875d33dad692af0274b0ddb33b
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-04-27 16:02:30 +0100

    Remove existing CSS files and create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './components/home/home';
 import Newsletters from './components/newsletters/newsletters';
+import Navbar from './components/navbar/navbar';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/newsletters" element={<Newsletters />} />
