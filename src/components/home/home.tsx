4c42a1b9c85ad9cc7e494399fde0ad11ca452925
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-04-28 15:07:35 +0100

    Fix css on homepage

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -7,24 +7,21 @@ const Home:FC = () => {
 
     return (
     <>
-    <div className="mt-90 mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-30 lg:flex lg:px-8 lg:pt-40">
-      <div className="grid grid-cols-2 gap-4">
-        <div>
-        <h1 className="mt-100 mb-4 text-6xl">Journals.GG</h1>
-        <h2 className="mb-10">We publish a selection of newsletters covering the world of emerging investments.
-            We keep tens of thousands of engaged investors ahead of the curve - from stocks, to crypto and beyond.
-            No BS, no fluff and no spam - We help our readers become a better investor in 5 minutes per day.</h2>
-        </div>
-  
-        <div>
-          <img src={phone}></img>
+      <div className="mt-90 mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-30 lg:flex lg:px-8 lg:pt-40">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="mb-4 text-6xl text-center">Journals.GG</h1>
+            <h2 className="mb-10 text-center">We publish a selection of newsletters covering the world of emerging investments. We keep tens of thousands of engaged investors ahead of the curve - from stocks, to crypto and beyond. No BS, no fluff and no spam - We help our readers become a better investor in 5 minutes per day.</h2>
+          </div>
+          <div>
+            <img src={phone}></img>
+          </div>
         </div>
       </div>
-     </div>
-    <div className="mb-40">
+      <div className="mb-40">
      <Companies />
     </div>
-  </>
+    </>
       );
 }
 
