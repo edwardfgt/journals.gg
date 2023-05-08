9a3c0f0452cb4f3ee49dd1312316b14d78bf8249
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-05-08 22:24:13 +0100

    Small styling changes to contact form

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -47,10 +47,10 @@ const Contact: FC = (): ReactElement => {
     
     }
     return (
-            <section className="bg-white dark:bg-gray-900">
-            <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Say hi</h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Alternatively, send us an email mail@journals.gg</p>
+        <section className="min-h-screen bg-white dark:bg-gray-900 flex flex-col items-center justify-center">
+        <div className="px-4 mx-auto max-w-2xl">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Say hi</h2>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Alternatively, send us an email mail@journals.gg</p>
                 <form action="#" className="space-y-8" onSubmit={handleSubmit}>
                     <div>
                         <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
